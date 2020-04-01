96576905c8bd1b84181486aca13dd8c9ce3d899a
Soner Eren <sonereren39@gmail.com>
2020-04-01 17:02:12 +0300

    Add files via upload
     Single Number solution -30 days coding challenge day1

---
+++ Algorithm/136. Single Number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+	public int singleNumber(int[] nums) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (set.contains(nums[i])) {
+				set.remove(nums[i]);
+			} else {
+				set.add(nums[i]);
+			}
+		}
+		return (set.stream().findFirst().get()).intValue();
+	}
+}
