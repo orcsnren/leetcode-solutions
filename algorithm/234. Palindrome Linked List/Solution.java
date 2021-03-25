18498e041d288eca0fbd1573f6ef1d3c4b1ef89b
Soner <Soner@Soner-PC>
2021-03-25 22:40:31 +0300

    234. problem solution

---
+++ algorithm/234. Palindrome Linked List/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+	public boolean isPalindrome(ListNode head) {
+		ArrayList<Integer> list = new ArrayList();
+		while (head != null) {
+			list.add(head.val);
+			head = head.next;
+		}
+		int start = 0;
+		int end = list.size() - 1;
+		while (start < end) {
+			if (!list.get(start).equals(list.get(end)))
+				return false;
+
+			start++;
+			end--;
+		}
+		return true;
+	}
+}
