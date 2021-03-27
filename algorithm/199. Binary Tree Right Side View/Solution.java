92a8f0033633874f546eecc7808736635fb48f9e
Soner <Soner@Soner-PC>
2021-03-28 00:06:54 +0300

    199 problem solution

---
+++ algorithm/199. Binary Tree Right Side View/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+		traverse(root, list, 0);
+		return list;
+	}
+
+	public void traverse(TreeNode root, List<Integer> list, int level) {
+		if (root == null)
+			return;
+
+		if (list.size() <= level)
+			list.add(root.val);
+
+		traverse(root.right, list, level + 1);
+		traverse(root.left, list, level + 1);
+
+	}
+}
